2049dfa84468ff5469883962483d726ecab164a4
unknown <jonathancalderonsilberman@gmail.com>
2021-03-29 10:23:17 -0700

    saving lock

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Signup from './Signup';
-import Login from '/Login';
+import Login from './Login';
 import HomePage from './HomePage';
 
 const NavBar = () => {
@@ -12,11 +11,6 @@ const NavBar = () => {
                 <Link to='/login'>Login</Link>
                 <Link to='/signup'>Sign Up</Link>
             </nav>
-            <Router>
-                <Route path='/signup' component={Signup} />
-                <Route path='/login' component={Login} />
-                <Route exact path='/' component={HomePage} />
-            </Router>
         </div>
     )
 }
