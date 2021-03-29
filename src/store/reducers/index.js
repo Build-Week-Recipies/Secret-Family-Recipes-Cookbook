b947518c2a82aec1436336361be9f21724dc06ef
unknown <jonathancalderonsilberman@gmail.com>
2021-03-29 09:36:21 -0700

    more setting up

---
+++ src/store/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    isLoading: false
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
