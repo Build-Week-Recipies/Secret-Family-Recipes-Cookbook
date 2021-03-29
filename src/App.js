a05c44135d13e7efd25797fc342d7bfac5e8e36c
samplesn08 <samplesn08@gmail.com>
2021-03-29 13:19:38 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import HomePage from './components/HomePage';
