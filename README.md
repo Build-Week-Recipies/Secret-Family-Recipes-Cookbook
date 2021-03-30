fdcd686af387055b297dc3a0c0d445390ea326c0
samplesn08 <samplesn08@gmail.com>
2021-03-30 11:21:28 -0400

    styled and animated the nav

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Secret-Family-Recipes-Cookbook
 The little cards my grandma wrote the recipes on in her beautiful cursive are getting lost or are hard to read. I need somewhere secure to keep my recipes with me at all times!
+
+* Be sure to install dependencies with 'npm install', then launch with 'npm start'
+
+* Unit 3 - Jonathan Calderon, Nicholas Samples
+* Unit 4 - Francis Bonacic
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import '../App.css';
+
+//styles
+const StyledLink = styled(Link)`
+    color: rgb(134, 64, 64);
+    text-decoration: none;
+    letter-spacing: 3px;
+    font-weight: bold;
+`;
+//end styles
 
 // const initialDisplay = {
 //     login: false,
@@ -10,22 +21,50 @@ import { Link } from 'react-router-dom';
 //     home: false,
 // }
 
+
 const NavBar = (props) => {
+    useEffect(() => {
+    //animation styles
+        const navSlide = () => {
+            const burger = document.querySelector('.burger');
+            const nav = document.querySelector('.nav-links');
+            const navLinks = document.querySelectorAll('.nav-links li');
+
+            burger.addEventListener('click', () => {
+                nav.classList.toggle('nav-active');
+                navLinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = '';
+                    } else {
+                        link.style.animation = `navLinkFade .5s ease forwards ${index / 7 + .5}s`;
+                    }
+                })
+                burger.classList.toggle('toggle');
+            })
+        }
+        navSlide();
+        //end styles
+    }, [])
+
     const { display } = props;
     const [show] = useState(display)
-
+    
     return (
         <div>
             <nav>
-                {show.home === true && <Link to='/'>Home</Link>}
-                {show.add === true && <Link to='/dashboard/add'>Add</Link>}
-                {show.signUp === true && < Link to='/signup'>Sign Up</Link>}
-                {show.login === true && <Link to='/login'>Login</Link>}
-                {show.logOut === true && <Link to='/'>Log Out</Link>}
-
-
+                <ul className='nav-links'>
+                    <li>{show.home === true && <StyledLink to='/'>Home</StyledLink>}</li>
+                    <li>{show.signUp === true && < StyledLink to='/signup'>Sign Up</StyledLink>}</li>
+                    <li>{show.login === true && <StyledLink to='/login'>Login</StyledLink>}</li>
+                    <li>{show.add === true && <StyledLink to='/dashboard/add'>Add</StyledLink>}</li>
+                    <li>{show.logOut === true && <StyledLink to='/'>Log Out</StyledLink>}</li>
+                </ul>
+                <div className='burger'>
+                    <div className='line1'></div>
+                    <div className='line2'></div>
+                    <div className='line3'></div>
+                </div>
             </nav>
-
         </div >
     )
 }
